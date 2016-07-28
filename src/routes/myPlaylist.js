162782ec98de205fa8f6607df176cbafa2afb41d
mcelearr <rorymcelearney@gmail.com>
2016-07-28 12:15:33 +0100

    logs request

--- src/routes/myPlaylist.js
+++ src/routes/myPlaylist.js
@@ -4,6 +4,7 @@ module.exports = {
   path: '/myPlaylist',
   method: 'GET',
   handler: (request, reply) => {
+    console.log(request);
     reply.view('home', {myVar: ['some content','some more content','even more content']})
   }
 };
