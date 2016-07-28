d309d3691db545f524bd59f588c055b4d7879f12
Matt <matthewsharp123@gmail.com>
2016-07-28 22:49:40 +0100

    add to playlist functionality

--- src/helpers/addToPlaylist.js
+++ src/helpers/addToPlaylist.js
@@ -2,8 +2,9 @@
 
 const request = require('request');
 
-module.exports = (trackId, userId) => {
+module.exports = (userId, trackId) => {
   return (err, response, body, playlistId) => {
+    console.log('Got to add to playlist');
     let playlist = '';
     if (!playlistId) {
       playlist = body.playlistId;
@@ -13,7 +14,7 @@ module.exports = (trackId, userId) => {
     const data = {
       headers: response.request.headers,
     };
-    request.post(`https://api.spotify.com/v1/users/${userId}/playlists/${playlist}/tracks?uris:spotify.track:${trackId}`, data, (e, r, b) => {
+    request.post(`https://api.spotify.com/v1/users/${userId}/playlists/${playlist}/tracks?uris=spotify:track:${trackId}`, data, (e, r, b) => {
       console.log(b);
     });
   };
--- src/helpers/createPlaylist.js
+++ src/helpers/createPlaylist.js
@@ -4,15 +4,17 @@ const request = require('request');
 
 module.exports = (addToPlaylist) => {
   return (userId, response) => {
+    console.log('Got to create Playlist');
     const header  = response.request.headers;
-    header['Content-Type'] = 'application.json'  ;
+    header['Content-Type'] = 'application/json';
+    
     const data = {
-      form: {
-        name: 'My Discover Weekly Picks',
+      form: JSON.stringify({ name: 'My Discover Weekly Picks',
         public: true,
-      },
+      }),
       headers: header,
     };
+    console.log(data);
     request.post(`https://api.spotify.com/v1/users/${userId}/playlists`, data, addToPlaylist);
   };
 };
